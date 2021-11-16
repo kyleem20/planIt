b0c2833558e4a7ca245f628b9bb1dd6a96d7c690
Liz Madsen <cyberneticdenim@gmail.com>
2021-11-16 12:27:37 -0700

    Task Updates

---
+++ planIt.client/src/components/AddSprintModal.vue
@@ -0,0 +1,50 @@
+<template>
+  <Modal id="post-modal">
+    <template #modal-title>
+      <h4>Make A New Post</h4>
+    </template>
+    <template #modal-body>
+      <form @submit.prevent="handleSubmit">
+        <div class="justify-content-around">
+          <div>
+            <label for="body" class="form-label"> Body</label>
+            <textarea
+              type="text"
+              class="form-control"
+              name="body"
+              id="body"
+              placeholder="Body"
+              min="10"
+              max="5000"
+              required
+              v-model="post.body"
+            >
+            </textarea>
+          </div>
+          <div>
+            <label for="image" class="form-label"> Image URL</label>
+            <input
+              type="text"
+              class="form-control"
+              name="image"
+              id="image"
+              placeholder="Image"
+              required
+              v-model="post.imgUrl"
+            />
+          </div>
+        </div>
+        <div class="modal-footer">
+          <button
+            type="button"
+            class="btn btn-secondary text-light"
+            data-bs-dismiss="modal"
+          >
+            Close
+          </button>
+          <button type="submit" class="btn btn-success">Submit</button>
+        </div>
+      </form>
+    </template>
+  </Modal>
+</template>
