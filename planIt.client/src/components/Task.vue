e15e5161b76b2dd1eec8f0e8bb4e861a4b8c1e4b
Liz Madsen <cyberneticdenim@gmail.com>
2021-11-17 16:17:09 -0700

    Task Updates

--- planIt.client/src/components/Task.vue
+++ planIt.client/src/components/Task.vue
@@ -1,84 +1,90 @@
 <template>
   <div class="row" align="left">
-    <div class="form-check">
-      <label class="form-check-label ms-3">
-        <input
-          type="checkbox"
-          class="form-check-input"
-          name=""
-          id=""
-          value="checkedValue"
-          unchecked
-        />
-        <!--TODO {{task.name}} -->
-        <h6>
-          <b>TASK NAME</b>
-        </h6>
-      </label>
-      <!-- <p> Created - DATE | Completed - DATE</p> -->
-      <button
-        class="btn"
-        data-bs-toggle="offcanvas"
-        href="#notesOffCanvas"
-        rolw="button"
-        aria-controls="notesOffCanvas"
-      >
-        0 <img class="commentImg" src="https://i.imgur.com/rlHQekg.png" />
-      </button>
-      <div
-        class="offcanvas offcanvas-end"
-        tabindex="-1"
-        id="notesOffCanvas"
-        aria-labelledby="notesOffCanvasLabel"
-      >
-        <div class="offcanvas-header">
-          <h5 class="offcanvas-title" id="notesOffCanvasLabel">
-            SPRINT NAME > TASK NAME
-          </h5>
-          <!--  TODO {{sprint.name}} > {{task.name}} -->
-          <button
-            type="button"
-            class="btn-close text-reset"
-            data-bs-dismiss="offcanvas"
-            aria-label="Close"
-          ></button>
-        </div>
-        <hr class="m-0" />
-        <div class="row offcanvas-body d-flex">
-          <div class="col-12">
-            <p class="projectsText"><b>Status</b></p>
-            <div class="row">
-              <div class="col-12 d-flex justify-content-around">
-                <p
-                  class="rounded-pill pendingPill text-light text-center"
-                  align="middle"
-                >
-                  Pending
-                </p>
-                <p
-                  class="rounded-pill progressPill text-light text-center"
-                  align="middle"
-                >
-                  In Progress
-                </p>
-                <!-- TODO v-if here -->
-                <p class="rounded-pill completedPill">Done</p>
+    <div class="col-12 d-flex">
+      <div class="form-check">
+        <label class="form-check-label ms-3">
+          <input
+            type="checkbox"
+            class="form-check-input"
+            name=""
+            id=""
+            value="checkedValue"
+            unchecked
+          />
+          <!--TODO {{task.name}} -->
+          <h6>
+            <b>TASK NAME</b>
+          </h6>
+        </label>
+        <!-- <p> Created - DATE | Completed - DATE</p> -->
+        <button
+          class="btn"
+          data-bs-toggle="offcanvas"
+          href="#notesOffCanvas"
+          rolw="button"
+          aria-controls="notesOffCanvas"
+        >
+          0 <img class="commentImg" src="https://i.imgur.com/rlHQekg.png" />
+        </button>
+        <div
+          class="offcanvas offcanvas-end"
+          tabindex="-1"
+          id="notesOffCanvas"
+          aria-labelledby="notesOffCanvasLabel"
+        >
+          <div class="offcanvas-header">
+            <h5 class="offcanvas-title" id="notesOffCanvasLabel">
+              SPRINT NAME > TASK NAME
+            </h5>
+            <!--  TODO {{sprint.name}} > {{task.name}} -->
+            <button
+              type="button"
+              class="btn-close text-reset"
+              data-bs-dismiss="offcanvas"
+              aria-label="Close"
+            ></button>
+          </div>
+          <hr class="m-0" />
+          <div class="row offcanvas-body d-flex">
+            <div class="col-12">
+              <p class="projectsText"><b>Status</b></p>
+              <div class="row">
+                <div class="col-12 d-flex justify-content-around">
+                  <p
+                    class="rounded-pill pendingPill text-light text-center"
+                    align="middle"
+                  >
+                    Pending
+                  </p>
+                  <p
+                    class="rounded-pill progressPill text-light text-center"
+                    align="middle"
+                  >
+                    In Progress
+                  </p>
+                  <!-- TODO v-if here -->
+                  <p class="rounded-pill completedPill">Done</p>
+                </div>
               </div>
-            </div>
-            <div class="row">
-              <div class="col-12 text-center notesText">Notes</div>
-            </div>
-            <hr />
-            <AddNote />
-            <div>
-              <Note />
+              <div class="row">
+                <div class="col-12 text-center notesText">Notes</div>
+              </div>
+              <hr />
+              <AddNote />
+              <div>
+                <Note />
+              </div>
             </div>
           </div>
         </div>
-      </div>
 
-      0
-      <img src="https://i.imgur.com/P7nHMkP.png" class="weight" />
+        0
+        <img src="https://i.imgur.com/P7nHMkP.png" class="weight" />
+      </div>
+      <button class="btn deleteTask">
+        Delete Task &nbsp;&nbsp;
+        <img class="trashCan" src="https://i.imgur.com/SHjFXfJ.png" />
+      </button>
     </div>
   </div>
 </template>
@@ -123,6 +129,18 @@ export default {
   font-size: 1.25rem;
   font-family: Impact, "Arial Narrow Bold", sans-serif;
   background: -webkit-linear-gradient(#013571, #aec6ee);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+.trashCan {
+  height: 25px;
+}
+.deleteTask {
+  width: 150px;
+  border: none;
+  line-height: 1;
+  font-family: Impact, "Arial Narrow Bold", sans-serif;
+  background: -webkit-linear-gradient(#f7b9ff, #844586);
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
 }
