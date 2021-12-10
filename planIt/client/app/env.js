a4e8815e435b6a7267c26393437f25579ed04e54
Kylee <kylee.j.marlin@gmail.com>
2021-12-10 16:29:38 -0700

    heroku updates

--- planIt/client/app/env.js
+++
@@ -1,5 +0,0 @@
-export const dev = window.location.origin.includes('localhost')
-export const baseURL = dev ? 'http://localhost:3000' : ''
-export const domain = 'kylee-marlin.us.auth0.com'
-export const audience = 'https://CoolStuff.com'
-export const clientId = 'mpXgASnPybMqw6Qp3lKxL9QNqXVp7ylk'
