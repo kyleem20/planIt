f48c1892503593f42d1ba6bdaa83bf9d780e99f5
Kylee <kylee.j.marlin@gmail.com>
2021-12-10 16:22:38 -0700

    vite

--- planIt.client/vite.config.js
+++ planIt.client/vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [vue()],
   build: {
-    outDir: 'docs',
+    outDir: '../planIt/client',
     sourcemap: false
   },
   server: {
