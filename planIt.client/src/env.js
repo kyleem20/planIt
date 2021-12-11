9f462846894cf60da3246508dbc361a22405fbe1
Kylee <kylee.j.marlin@gmail.com>
2021-12-10 17:09:11 -0700

    bullfrog

--- planIt.client/src/env.js
+++ planIt.client/src/env.js
@@ -1,6 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
-export const domain = 'kylee-marlin.us.auth0.com'
-export const audience = 'https://CoolStuff.com'
-export const clientId = 'mpXgASnPybMqw6Qp3lKxL9QNqXVp7ylk'
+export const domain = 'dev-uxhb84bx.us.auth0.com'
+export const audience = 'https://kylee-planit.herokuapp.com'
+export const clientId = 'QtzFh9AGwi5b6u00271tNzMAbeYKUv2j'
